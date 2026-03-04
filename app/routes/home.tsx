f7f4580ddafd3a9f7a9d6b0cb58516f50e5c0a93
Diane-div <divyanshi_230178@aitpune.edu.in>
2026-03-04 14:48:07 +0530

    ScoreCards created

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -15,16 +16,18 @@ export default function Home() {
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-10">
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2> Review your submissions and check AI-powered feedback.</h2>
         </div>
-      </section>
-      {resumes.map((resume) => (
-          <div>
-              <h1>{resume.jobTitle}</h1>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
           </div>
-      ))}
+        )}
+      </section>
     </main>
   );
 }
