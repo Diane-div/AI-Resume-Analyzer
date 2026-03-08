e68d1a0bf6d4e0dd48bfd90f8a6ddc470eb2a5f3
Diane-div <divyanshi_230178@aitpune.edu.in>
2026-03-08 19:03:18 +0530

    Feedback initiation

---
+++ app/routes/upload.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "~/components/Navbar";
+
+const Upload = () => {
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <Navbar />
+            <section className="main-section">
+            </section>
+        </main>
+    )
+}
+export default Upload
